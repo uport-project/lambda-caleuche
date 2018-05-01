e0d905dd736d7fc924f86600e432c159059a48d1
Pelle Braendgaard <pelle@stakeventures.com>
2018-04-30 18:23:07 -0600

    Updated to use did-jwt

--- src/lib/uPortMgr.js
+++ src/lib/uPortMgr.js
@@ -1,27 +1,12 @@
-//import { JWT } from 'uport'
-import UportLite from 'uport-lite'
-import { verifyJWT } from 'uport/lib/JWT'
+require('ethr-did-resolver')()
+require('uport-did-resolver')()
+import { verifyJWT } from "did-jwt/lib/JWT";
 
 class UportMgr {
 
-    constructor (settings = {}) {
-        this.settings = settings
-        const registry =  new UportLite()
-
-        this.settings.registry = (address) => new Promise((resolve, reject) => {
-            registry(address, (error, profile) => {
-                if (error) return reject(error)
-                resolve(profile)
-            })
-        })
-    }
-
-
-    async verifyToken(token){
-        if(!token) throw('no token')
-        return verifyJWT(this.settings,token)
-    }
-
-
+  async verifyToken(token) {
+    if (!token) throw "no token";
+    return verifyJWT(token);
+  }
 }
 module.exports = UportMgr;
